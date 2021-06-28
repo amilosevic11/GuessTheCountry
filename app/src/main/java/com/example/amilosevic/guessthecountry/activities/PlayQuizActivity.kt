b5634aa07ec105a9435565dcc10ef513a854f60d
Gauss <gauss@Gausss-Mac-mini.local>
2021-06-28 11:45:25 +0200

    Image (flag) not loading

--- app/src/main/java/com/example/amilosevic/guessthecountry/activities/PlayQuizActivity.kt
+++ app/src/main/java/com/example/amilosevic/guessthecountry/activities/PlayQuizActivity.kt
@@ -1,13 +1,16 @@
 package com.example.amilosevic.guessthecountry.activities
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.example.amilosevic.guessthecountry.R
 import com.example.amilosevic.guessthecountry.databinding.ActivityPlayQuizBinding
 import com.example.amilosevic.guessthecountry.viewmodels.PlayQuizViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.random.Random
 
 class PlayQuizActivity : AppCompatActivity() {
 
@@ -22,7 +25,15 @@ class PlayQuizActivity : AppCompatActivity() {
 
         playQuizViewModel.getAllCountries()
         playQuizViewModel.myResponse.observe(this, {
+            binding.btnFirstAnswer.text = it[Random.nextInt(it.size)].name
+            binding.btnSecondAnswer.text = it[Random.nextInt(it.size)].name
+            binding.btnThirdAnswer.text = it[Random.nextInt(it.size)].name
+            binding.btnFourthAnswer.text = it[Random.nextInt(it.size)].name
 
+//            binding.ivFlag.setImageURI(Uri.parse(it[Random.nextInt(it.size)].flag))
+            Glide.with(this)
+                .load(it[Random.nextInt(it.size)].flag)
+                .into(binding.ivFlag)
         })
     }
 }
--- app/src/main/java/com/example/amilosevic/guessthecountry/viewmodels/PlayQuizViewModel.kt
+++ app/src/main/java/com/example/amilosevic/guessthecountry/viewmodels/PlayQuizViewModel.kt
@@ -5,17 +5,23 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.amilosevic.guessthecountry.data.repos.CountriesInfo
 import com.example.amilosevic.guessthecountry.data.repos.GuessTheCountryRepository
-import com.example.amilosevic.guessthecountry.data.repos.UserRepository
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class PlayQuizViewModel(private val repository: GuessTheCountryRepository) : ViewModel() {
 
     val myResponse : MutableLiveData<ArrayList<CountriesInfo>> = MutableLiveData()
+    private var countriesInfo : ArrayList<CountriesInfo>? = null
 
     fun getAllCountries() {
         viewModelScope.launch {
             val response = repository.getAllCountries()
             myResponse.postValue(response)
+            countriesInfo = response
         }
+    }
+
+    fun getRandomCountry() : String? {
+        return countriesInfo?.get(Random.nextInt())?.name
     }
 }
